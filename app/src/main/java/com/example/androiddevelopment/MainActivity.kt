5538cf604ca1bc4f349f14e9708a6aebfc43f918
sarbador <avaz.shams.2002@gmail.com>
2024-04-05 22:21:44 +0500

    first commit

---
+++ app/src/main/java/com/example/androiddevelopment/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.androiddevelopment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevelopment.ui.theme.AndroidDevelopmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidDevelopmentTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Avaz", Modifier.size(20.dp))
+                }
+            }
+            mainFunctionCode();
+
+        }
+    }
+}
+
+
+fun mainFunctionCode() {
+    println("hello brothers what is going on? ");
+}
+
+@Composable
+fun Greeting(name: String? = null, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidDevelopmentTheme {
+        Greeting("Android")
+    }
+}
