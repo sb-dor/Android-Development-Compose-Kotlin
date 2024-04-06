7f04ef0795144673ae5aee380e41c28ec25c17db
sarbador <avaz.shams.2002@gmail.com>
2024-04-06 21:19:51 +0500

    learning ui of jetpack compose

--- app/src/main/java/com/example/androiddevelopment/state_changing/StateChanging.kt
+++ app/src/main/java/com/example/androiddevelopment/state_changing/StateChanging.kt
@@ -1,14 +1,18 @@
 package com.example.androiddevelopment.state_changing
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
@@ -17,6 +21,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,6 +32,8 @@ import androidx.compose.ui.unit.sp
 @SuppressLint("MutableCollectionMutableState")
 @Composable
 fun StateChangingComposable() {
+
+    val focusManager = LocalFocusManager.current // for closing keyboard on outside
 
     val counter = remember {
         mutableIntStateOf(0)
@@ -33,10 +43,31 @@ fun StateChangingComposable() {
         mutableStateListOf<String>()
     }
 
+    var textFieldData = remember {
+        mutableStateOf("")
+    }
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus(); // for closing keyboard on outside
+                })
+            }, contentAlignment = Alignment.Center
+    ) {
         LazyColumn {
             item {
+
+                TextField(
+                    value = textFieldData.value,
+                    onValueChange = { it -> textFieldData.value = it },
+                    label = { Text(text = "Text field") },
+                    keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done), // keyboard enter type
+                )
+
                 ElevatedButton(onClick = { counter.intValue++ }) {
                     Text(text = "Tap counter", color = Color.White)
                 }
@@ -51,7 +82,9 @@ fun StateChangingComposable() {
                 }
 
                 Box(modifier = Modifier.height(10.dp))
-                Text(text = "${arrayOfData.toList()}", fontSize = 20.sp, textAlign = TextAlign.Center)
+                Text(
+                    text = "${arrayOfData.toList()}", fontSize = 20.sp, textAlign = TextAlign.Center
+                )
                 Box(modifier = Modifier.height(10.dp))
             }
 
